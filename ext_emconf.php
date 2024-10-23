e6b33c4cd06870a95a663853720957e42f611e28
Sven Wappler <wappler@wappler.systems>
2024-10-23 15:23:15 +0200

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '2.13.3',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.0.0-12.9.99',
+            'typo3' => '13.0.0-13.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
